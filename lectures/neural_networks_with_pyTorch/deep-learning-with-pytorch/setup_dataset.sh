2777939c6d1c1b99a8550e7ee98b156bcbd85c72
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-05-08 16:29:39 +0600

    The commit is successful

--- lectures/neural_networks_with_pyTorch/deep-learning-with-pytorch/setup_dataset.sh
+++ lectures/neural_networks_with_pyTorch/deep-learning-with-pytorch/setup_dataset.sh
@@ -5,7 +5,8 @@
 echo "1. Downloading dataset..."
 wget https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
 
-echo "2. Unzipping dataset..."
+echo "2. remove exisiting data and Unzipping dataset..."
+rm -rf Cat_Dog_data/
 unzip kagglecatsanddogs_5340.zip -d Cat_Dog_data
 
 echo "3. Organizing dataset using Python script..."
